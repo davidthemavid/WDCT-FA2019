1c00641790156a13e62d807b05a66e7a77c96f5e
Nooruddin Khorasi <brainstation@android-d3dc09f4b959c515.localdomain>
2019-12-05 14:57:07 -0800

    small typo while importing mysql

--- week-ten/day-three/sequelize-demo/index.js
+++ week-ten/day-three/sequelize-demo/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const config = require("./config/config.json");
 
 let connection;
