015f3556efb59cd0c69441f987869cee73280a5a
Nooruddin Khorasi <brainstation@android-d3dc09f4b959c515.localdomain>
2019-12-05 12:50:42 -0800

    deploying full stack app

--- week-ten/day-three/sequelize-demo/index.js
+++ week-ten/day-three/sequelize-demo/index.js
@@ -23,6 +23,15 @@ app.use("/api/units", require("./routes/api/unitsController"));
 //  routes for fruits
 app.use("/api/fruits", require("./routes/api/fruitsController"));
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
