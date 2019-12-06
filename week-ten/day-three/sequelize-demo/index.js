3d84d2892d2a4688a50d5d5b4ae7187f7ef7aba3
Nooruddin Khorasi <brainstation@android-d3dc09f4b959c515.localdomain>
2019-12-05 17:18:25 -0800

    removing connection to db from server

--- week-ten/day-three/sequelize-demo/index.js
+++ week-ten/day-three/sequelize-demo/index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
 const cors = require("cors");
-const mysql = require("mysql2");
-const config = require("./config/config.json");
+// const mysql = require("mysql2");
+// const config = require("./config/config.json");
 
-let connection;
+// let connection;
 
 //  initialize express for use
 const app = express();
@@ -27,12 +27,12 @@ app.use("/api/units", require("./routes/api/unitsController"));
 //  routes for fruits
 app.use("/api/fruits", require("./routes/api/fruitsController"));
 
-//make connection
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection(config.development);
-}
+// //make connection
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection(config.development);
+// }
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
@@ -46,6 +46,6 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
-connection.connect();
+// connection.connect();
 // Export connection for our ORM to use.
-module.exports = connection;
+// module.exports = connection;
